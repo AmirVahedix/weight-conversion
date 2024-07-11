2d26cf91753755c7b77d018ad2225969a04597be
Amir Vahedi <amirvahedix@gmail.com>
2024-07-12 00:42:49 +0330

    refatores

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -4,6 +4,5 @@ use AmirVahedix\WeightConversion\WeightConversion;
 
 it('can convert kilograms to pounds', function () {
     $pounds = WeightConversion::kilograms(1000)->toPounds();
-    
     expect($pounds)->toEqual(2204.6);
 });
