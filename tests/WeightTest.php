30f0de67a5c3daa4c1c044c91905e1d3d4e8eacd
Amir Vahedi <amirvahedix@gmail.com>
2024-07-12 21:24:35 +0330

    Added toStones()

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -3,6 +3,11 @@
 use AmirVahedix\WeightConversion\WeightConversion;
 
 it('can convert kilograms to pounds', function () {
-    $pounds = WeightConversion::kilograms(1000)->toPounds();
-    expect($pounds)->toEqual(2204.6);
+    $pounds = WeightConversion::kilograms(100)->toPounds();
+    expect($pounds)->toEqual(220.46);
+});
+
+it('can convert kilograms to stones', function () {
+    $stones = WeightConversion::kilograms(100)->toStones();
+    expect($stones)->toEqual(15.747);
 });
